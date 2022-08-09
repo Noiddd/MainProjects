1cbcea71292f79757ba3a9e9120cdd6b8cbd36ba
Noiddd <dionang11@gmail.com>
2022-08-09 11:43:31 +0800

    materialui

---
+++ material-ui/src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@mui/material";
+
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCameraIcon />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
